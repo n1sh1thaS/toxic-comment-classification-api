0ca88b376b14ffd2d834f2aed52b054e8aba0596
nselvakumar25 <nselvakumar@ucsd.edu>
2024-08-27 20:44:32 -0700

    create sign up form component and sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <SignUp />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import SignUpForm from "../components/SignUpForm";
+import { Typography, Grid } from "@mui/material";
+
+const SignUp = () => {
+  return (
+    <div>
+      <Grid container spacing={3}>
+        <Grid item xs={12}></Grid>
+        <Grid item xs={12} marginTop={18}>
+          <Typography
+            variant="h3"
+            align="center"
+            sx={{ fontSize: 35, marginBottom: 5 }}
+          >
+            Create Your Account
+          </Typography>
+          <Typography variant="h4" align="center">
+            <SignUpForm />
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default SignUp;
