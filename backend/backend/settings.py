c0313ad2cf628d2b3d684bc97d5910a9731ef402
nselvakumar25 <nselvakumar@ucsd.edu>
2024-08-31 12:38:46 -0700

    jwt authentication

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 from pathlib import Path
+from datetime import timedelta 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "djoser",
     "toxic_comment_classification_api",
 ]
 
@@ -128,3 +130,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
+}
+
+AUTH_USER_MODEL = 'toxic_comment_classification_api.User'
--- backend/toxic_comment_classification_api/models.py
+++ backend/toxic_comment_classification_api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=255)
+class User(AbstractUser):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
